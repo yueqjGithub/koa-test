59714208be9bf38e1776a332d7ae482806d96c23
yueqjGit <410281036@qq.com>
2021-09-01 16:47:22 +0800

    修改mysql连接方法

--- src/controllers/mysqlConfig.ts
+++ src/controllers/mysqlConfig.ts
@@ -9,23 +9,38 @@ const pool = Mysql.createPool({
 })
 
 export class ConnectionSql {
-  constructor () {}
-  /** 基本连接查询 */
+  connection:any
+  constructor () {
+  }
+  /** 基本连接查询
+   *  这里对connection的if判断很重要，不做判断会导致连接数过多，后面的连接查询均会失败
+   */
   queryInfo (sql:string):Promise<any> {
     return new Promise((resolve, reject)=>{
-      pool.getConnection((err, connection) => {
-        if (err) {
-          reject(err)
-        } else {
-          connection.query(sql, (err, val) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(val)
-            }
-          })
-        }
-      })
+      if (this.connection) {
+        this.connection.query(sql, (err, val) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(val)
+          }
+        })
+      } else {
+        pool.getConnection((err, connection) => {
+          if (err) {
+            reject(err)
+          } else {
+            this.connection = connection
+            connection.query(sql, (err, val) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(val)
+              }
+            })
+          }
+        })
+      }
     })
   }
   testQuery () {
